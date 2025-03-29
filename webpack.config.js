84d95c8df993754f77c1f3af9405685f42f3343f
Sergey Kostin <sergeyvladimirovish@gmail.com>
2025-03-30 00:51:20 +0100

    add sitemap file generation

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,35 @@
 const path = require('path');
+const fs = require("fs");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+
+// Define your website's URLs dynamically
+const urls = [
+    { loc: "https://catogrammer.com/", lastmod: "2025-02-19", changefreq: "monthly", priority: "1.0" },
+];
+
+// Generate XML sitemap
+const generateSitemap = () => {
+    const sitemapContent = `<?xml version="1.0" encoding="UTF-8"?>
+    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    ${urls
+        .map(
+        (url) => `
+        <url>
+            <loc>${url.loc}</loc>
+            <lastmod>${url.lastmod}</lastmod>
+            <changefreq>${url.changefreq}</changefreq>
+            <priority>${url.priority}</priority>
+        </url>`
+    ) .join("\n")}
+    </urlset>`;
+
+    fs.writeFileSync(path.join(__dirname, '.', "build", "sitemap.xml"), sitemapContent);
+    console.log("✅ Sitemap generated: ./build/sitemap.xml");
+};
+generateSitemap();
 
 module.exports = {
 
@@ -68,6 +96,8 @@ module.exports = {
             filename: '[name].style.css'
         }),
 
-        new CleanWebpackPlugin(),
+        new CleanWebpackPlugin({
+            cleanOnceBeforeBuildPatterns: ["**/*", '!sitemap.xml'],
+        }),
     ]
 }
